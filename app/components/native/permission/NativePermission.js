4168c7cd179b8ab6840b06b9788c80cd1656f5ac
yabeshan <beshan@bigmir.net>
2020-11-15 02:59:44 +0200

    simple camera

--- app/components/native/permission/NativePermission.js
+++ app/components/native/permission/NativePermission.js
@@ -17,12 +17,11 @@ const Component = () => {
 
 Component.getCameraStatus = async () => {
   const platform = CheckPermission.getPlatform()
-  console.log("__CheckPermission__",  )
-
-
   const type = (platform.indexOf('ios')>0)
     ? PERMISSIONS.IOS.CAMERA
     : PERMISSIONS.ANDROID.CAMERA
+return platform +"_____"+ type
+/*
   check(type)
   .then((result) => {
     return result
@@ -30,10 +29,11 @@ Component.getCameraStatus = async () => {
   .catch((error) => {
     return "error"
   });
-
+  */
 }
 
 Component.getCameraComp = async () => {
+/*
   const status = await Component.getCameraStatus()
   if ( status == "granted" ) {
     return (
@@ -46,7 +46,7 @@ Component.getCameraComp = async () => {
       />
     )
   }
-
+  */
   return (
     <Text style={{color:'red', fontSize:30}}>Need Camera permissions</Text>
   )
